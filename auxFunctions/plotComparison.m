49c13f62119692c55a181cf44c4cbb10e5fa641e
Magdalena Kaplan <mkaplan@kth.se>
2025-10-05 17:34:20 +0200

    Update plotComparison.m

--- auxFunctions/plotComparison.m
+++ auxFunctions/plotComparison.m
@@ -1,6 +1,5 @@
 function [fig,strain_num,stress_num] = plotComparison(directory,folderName,simulationName,strain_exp,stress_exp)
 % PLOTCOMPARISON Plots numerical and experimental curve for comparison
-%   Detailed explanation goes here
 
 % Read strain and stress from chosen simulation
 [strainStress_num,~,~,~]=extractResults(directory,folderName,simulationName);
@@ -24,4 +23,5 @@ plot(strain_num*1e2,stress_num*1e-6,plotOpts{:},'Numerical'); hold off;
 ylabel('Stress [MPa]')
 xlabel('Strain [%]')
 lgd=legend('Location','southeast','FontSize',14);
+
 end
